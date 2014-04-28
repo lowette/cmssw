302f52de4367f0d4bccb18bb47f1f7b1fcfd22cd
Annik <annik.olbrechts@cern.ch>
2014-04-28 18:29:26 +0200

    Configuration file adapted for siPixelRecHits

---
+++ RunSteps/Digitizer/test/DigitizerRecHits.py
@@ -0,0 +1,70 @@
+# Configuration file storing siPixelRecHits
+# Based on step2_DIGI_L1_DIGI2RAW_L1TrackTrigger_RAW2DIGI_RECO.py (reconstruction origin)
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtendedPhase2TkBE5DReco_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(20)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    secondaryFileNames = cms.untracked.vstring(),
+    fileNames = cms.untracked.vstring('file:DIGI.root')
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)  #Set this to true for full Trig and TimeReport
+)
+
+# Output definition
+
+process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    fileName = cms.untracked.string('file:siPixelRecHitOutput.root'),
+)
+
+# Additional output definition
+
+# Other statements
+process.mix.digitizers = cms.PSet(process.theDigitizersValid)
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
+
+# Path and EndPath definitions
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
+process.FEVTDEBUGoutput.outputCommands.extend([
+  'keep *_siPixelRecHits_*_*'
+]) 
+
+process.pixeltrackerlocalreco = cms.Sequence(process.siPixelClusters*process.siPixelRecHits)
+process.reconstruction_step = cms.Path(process.pixeltrackerlocalreco)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.reconstruction_step,process.FEVTDEBUGoutput_step)
+
+# customisation of the process.
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.combinedCustoms
+from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_phase2_BE5D 
+
+#call to customisation function cust_phase2_BE5D imported from SLHCUpgradeSimulations.Configuration.combinedCustoms
+process = cust_phase2_BE5D(process)
+
+# End of customisation functions
