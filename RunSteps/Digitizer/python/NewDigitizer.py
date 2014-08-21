93c4bed6ff207f43b725a35a2147490c9f3ed242
ndaci <daci.nadir@gmail.com>
2014-08-22 00:49:19 +0200

    Add initial version of NewDigitizer with NewConfiguration. Buggy versions. Fix needed. Stuff commented.

---
+++ RunSteps/Digitizer/python/NewDigitizer.py
@@ -0,0 +1,125 @@
+# Imports
+import os, sys
+import FWCore.ParameterSet.Config as cms
+from Configuration.AlCa.GlobalTag import GlobalTag
+from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_phase2_BE5D
+
+# Default parameters
+n = -1
+input_file = os.path.dirname(os.path.realpath(sys.argv[1])) + '/../../Output/GEN_SIM.root'
+output_file = os.path.dirname(os.path.realpath(sys.argv[1])) + '/../../Output/DIGI.root'
+
+# Look for updates in the parameters using the program's input
+for i in range(2, len(sys.argv)):
+    if (sys.argv[i] == '_n' and len(sys.argv) > i + 1 and sys.argv[i+1][0] != '_'):
+        n = int(sys.argv[i+1])         
+        i += 1
+    elif (sys.argv[i] == '_output' and len(sys.argv) > i + 1 and sys.argv[i+1][0] != '_'):
+        output_file = sys.argv[i+1]
+        i += 1
+    elif (sys.argv[i] == '_input' and len(sys.argv) > i + 1 and sys.argv[i+1][0] != '_'):
+        input_file = sys.argv[i+1]
+        i += 1
+
+# Greetings
+print '------------------------------------------------------------'
+print '-- Running the Digitizer step with the following arguments:'
+print '-- Number of events: ' + str(n) 
+print '-- Input file: ' + input_file
+print '-- Output file: ' + output_file
+print '------------------------------------------------------------'
+
+# Create a new CMS process
+process = cms.Process('DIGI')
+
+# Import all the necessary files
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('RunSteps.Digitizer.NewConfiguration')
+process.load('Configuration.Geometry.GeometryExtendedPhase2TkBE5DReco_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('L1Trigger.TrackTrigger.TrackTrigger_cff')
+process.load('SimTracker.TrackTriggerAssociation.TrackTriggerAssociator_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# Number of events (-1 = all)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(n)
+)
+
+########################################################
+##################### Input files ######################
+########################################################
+
+## standard choice:input_file parameter is a name of file
+myfileNames = cms.untracked.vstring('file:'+input_file)
+
+## input_file is a tag to call a whole set of input files
+from RunSteps.Digitizer.files_tti_zmm_cfi import *
+
+if input_file=="tti_zmm":
+    myfileNames = tti_zmm
+
+## define the source
+process.source = cms.Source("PoolSource",fileNames = myfileNames)
+
+########################################################
+
+# Options
+process.options = cms.untracked.PSet()
+
+# Metadata (info)
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 0.1 $'),
+    annotation = cms.untracked.string('RunSteps Digitizer'),
+    name = cms.untracked.string('Applications')
+)
+
+# TAG
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
+
+# Output
+process.FEVTDEBUGoutput = cms.OutputModule(
+    "PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    fileName = cms.untracked.string('file:' + output_file),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RAW')
+        )
+    )
+
+# Steps
+print "==> DEFINE process.digitisation_step = cms.Path(process.pdigi) <=="
+
+process.digitisation_step = cms.Path(process.pdigi)
+
+print "==> DEFINED <=="
+
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+
+process.L1TrackTrigger_step = cms.Path(process.TrackTriggerClustersStubs)
+
+process.L1TTAssociator_step = cms.Path(process.TrackTriggerAssociatorClustersStubs)
+
+process.endjob_step = cms.EndPath(process.endOfProcess)
+
+process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
+
+# Processes to run
+print "==> SCHEDULE <=="
+#process.schedule = cms.Schedule(process.digitisation_step, process.L1simulation_step, process.digi2raw_step, process.L1TrackTrigger_step, process.L1TTAssociator_step, process.endjob_step, process.FEVTDEBUGoutput_step)
+
+process.schedule = cms.Schedule(process.digitisation_step, process.digi2raw_step, process.L1TrackTrigger_step, process.L1TTAssociator_step, process.endjob_step, process.FEVTDEBUGoutput_step)
+
+print "==> cust_phase2_BE5D <=="
+#process = cust_phase2_BE5D(process)
